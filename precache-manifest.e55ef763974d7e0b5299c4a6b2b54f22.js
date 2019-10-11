48ea00cb281feddba1a7557400cc785b23a1442e
carreterosanti <unicen123>
2019-10-10 23:36:40 -0300

    Updates

---
+++ precache-manifest.e55ef763974d7e0b5299c4a6b2b54f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c18c0565cabdd4a3251db7706a604df",
+    "url": "/Test-Proyecto-Final/index.html"
+  },
+  {
+    "revision": "e1ccf69622e9e81c05ab",
+    "url": "/Test-Proyecto-Final/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e40882e84135518d9f16",
+    "url": "/Test-Proyecto-Final/static/js/2.f6385795.chunk.js"
+  },
+  {
+    "revision": "e1ccf69622e9e81c05ab",
+    "url": "/Test-Proyecto-Final/static/js/main.ae1b0844.chunk.js"
+  },
+  {
+    "revision": "a0d029e9833d7b624520",
+    "url": "/Test-Proyecto-Final/static/js/runtime-main.11b4806c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Test-Proyecto-Final/static/media/logo.25bf045c.svg"
+  }
+]);
